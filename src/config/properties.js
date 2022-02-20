a868b5fb71e3cc9b55b04be0502bacaf4a397537
GauthamRaj18 <nandugr27@gmail.com>
2022-02-20 22:00:46 +1100

    fix: correct DB connection string added

--- src/config/properties.js
+++ src/config/properties.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: 8000,
-    DB:'mongodb://mongo:27017/clinicals',
+    DB_LOCAL:'mongodb://mongo:27017/clinicals',
     BASEURL: 'http://localhost/clinicalsapi',
-    DB_LOCAL:'mongodb://localhost/clinicals',
+    DB:'mongodb://localhost/clinicals',
 
 }
