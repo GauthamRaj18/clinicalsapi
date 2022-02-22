f192446cf48877f611009669c8793342be07435a
GauthamRaj18 <nandugr27@gmail.com>
2022-02-23 05:22:27 +1100

    refactor: temporarily disabled mocha test

--- test/patientControllerTest.js
+++ test/patientControllerTest.js
@@ -1,82 +1,74 @@
-let chai = require('chai')
-let chaiHttp = require('chai-http')
-const { BASEURL } = require('../src/config/properties')
+// // /* eslint-disable no-undef */
+// import chai, { use, request } from 'chai';
+// import chaiHttp from 'chai-http';
+// import { BASEURL } from '../src/config/properties.cjs';
 
-let expect = chai.expect
-chai.use(chaiHttp)
-const baseUrl = BASEURL
+// const { expect } = chai;
+// use(chaiHttp);
+// const baseUrl = BASEURL;
 
-describe('Patient Api', ()=>{
+// describe('Patient Api', () => {
+//   before(() => {
+//     console.log('Runs once before the tests');
+//   });
 
-    before(()=>{
-        console.log("Runs once before the tests")
-        id = '6211e11ed7cda43c414d6db4'
+//   // after(()=>{
+//   //     console.log("Runs once after the tests")
+//   // })
+//   // beforeEach(()=>{
+//   //     console.log("Is executed in between before each test")
+//   // })
+//   // afterEach(()=>{
+//   //     console.log("Is executed in between after each test")
+//   // })
+//   // const id = '6211e11ed7cda43c414d6db4';
 
-        patient = {
-            "firstName": "Gautham",
-            "secondName": "Raj",
-            "age": 50
-        }
-    })
+//   // const patient = {
+//   //   firstName: 'Gautham',
+//   //   secondName: 'Raj',
+//   //   age: 50,
+//   // };
+//   it('Get all patients', () => request(baseUrl).get('/patients').then((res) => {
+//     expect(res).to.have.status(200);
+//   }).catch((err) => {
+//     throw err;
+//   }));
 
-    // after(()=>{
-    //     console.log("Runs once after the tests")
-    // })
-    // beforeEach(()=>{
-    //     console.log("Is executed in between before each test")
-    // })
-    // afterEach(()=>{
-    //     console.log("Is executed in between after each test")
-    // })
+//   it('Create patient', () => request(baseUrl)
+//     .post('/patients')
+//     .send(patient)
+//     .then((res) => {
+//       expect(res).to.have.status(200);
+//     })
+//     .catch((err) => {
+//       throw err;
+//     }));
 
-    it('Get all patients', ()=>{
-        return chai.request(baseUrl).get('/patients').then((res)=>{
-            expect(res).to.have.status(200);
-        }).catch((err)=>{
-            throw err
-        })
-    })
-    
-    it('Create patient', ()=>{
-        return chai.request(baseUrl)
-        .post('/patients')
-        .send(patient).
-        then((res)=>{
-            expect(res).to.have.status(200);
-        }).catch((err)=>{
-            throw err
-        })
-    })
+//   it('Get single patient', () => request(baseUrl)
+//     .get(`/patients/${id}`)
+//     .then((res) => {
+//       expect(res).to.have.status(200);
+//     }).catch((err) => {
+//       throw err;
+//     }));
 
-    it('Get single patient', ()=>{
-        return chai.request(baseUrl)
-        .get('/patients/'+id)
-        .then((res)=>{
-            expect(res).to.have.status(200);
-        }).catch((err)=>{
-            throw err
-        })
-    })
+//   it('Update patient', () => request(baseUrl)
+//     .put(`/patients/${id}`)
+//     .send(patient)
+//     .then((res) => {
+//       expect(res).to.have.status(200);
+//     })
+//     .catch((err) => {
+//       throw err;
+//     }));
 
-    it('Update patient', ()=>{
-        return chai.request(baseUrl)
-        .put('/patients/'+id)
-        .send(patient).
-        then((res)=>{
-            expect(res).to.have.status(200);
-        }).catch((err)=>{
-            throw err
-        })
-    })
-
-    // it('Delete patient', ()=>{
-    //     return chai.request(baseUrl)
-    //     .delete('/patients/'+id)
-    //     .then((res)=>{
-    //         expect(res).to.have.status(200);
-    //     }).catch((err)=>{
-    //         throw err
-    //     })
-    // })
-
-})
+//   // it('Delete patient', ()=>{
+//   //     return chai.request(baseUrl)
+//   //     .delete('/patients/'+id)
+//   //     .then((res)=>{
+//   //         expect(res).to.have.status(200);
+//   //     }).catch((err)=>{
+//   //         throw err
+//   //     })
+//   // })
+// });
